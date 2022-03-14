28e91acc7bf83eb75920a00445f3f5f417a872d5
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-14 17:02:11 +0100

    A04 solved

--- A04/projections.cpp
+++ A04/projections.cpp
@@ -6,11 +6,9 @@
 #include<math.h>
 //(float)cos(b * M_PI / 180.0)
 //(float)sin(b * M_PI / 180.0)
-
-
 float n=-4,f=12,w=2;
-glm::mat4 rotation45=glm::mat4((float)cos(45 * M_PI / 180.0),0,(float)-sin(45 * M_PI / 180.0),0, 0,1,0,0, (float)sin(45 * M_PI / 180.0),0,(float)cos(45 * M_PI / 180.0),0, 0,0,0,1);
 float l=-1*w,r=w;
+glm::mat4 rotation45=glm::mat4((float)cos(45 * M_PI / 180.0),0,(float)-sin(45 * M_PI / 180.0),0, 0,1,0,0, (float)sin(45 * M_PI / 180.0),0,(float)cos(45 * M_PI / 180.0),0, 0,0,0,1);
 
 // Create a matrix for Isometric projection with the given aspect ratio a
 glm::mat4 PO1(float a) {
@@ -26,7 +24,7 @@ glm::mat4 PO2(float a) {
     float alpha=45;
     float t=(w/a),b=-1*(w/a);
 	glm::mat4 out = glm::mat4(1.0);
-    out=glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::ortho(l,r,b,t,n,f)*glm::rotate(glm::mat4(1),(float)(45 * M_PI / 180.0),glm::vec3(1,0,0))*rotation45;
+    out=glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::ortho(l,r,b,t,n,f)*glm::rotate(glm::mat4(1),(float)(alpha * M_PI / 180.0),glm::vec3(1,0,0))*rotation45;
     return out;
 }
 
@@ -35,19 +33,19 @@ glm::mat4 PO2(float a) {
 glm::mat4 PO3(float a) {
     int alpha=45, beta=60;
     float t=(w/a),b=-1*(w/a);
-
     glm::mat4 out = glm::mat4(1.0);
     out=glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::ortho(l,r,b,t,n,f)*glm::rotate(glm::mat4(1),(float)(alpha* M_PI / 180.0),glm::vec3(1,0,0))*glm::rotate(glm::mat4(1),(float)(beta* M_PI / 180.0),glm::vec3(0,1,0));
-
 	return out;
 }
 
-// Create a matrix for Cabinet projection (alpha = 45)
+// Create a matrix for Cabinet projection (alpha = 45, ro=1/2)
 // with the given aspect ration a
 glm::mat4 PO4(float a) {
     float t=(w/a),b=-1*(w/a);
-
+    float ro=0.5;
+    int alpha=45;
     glm::mat4 out = glm::mat4(1.0);
-	return out;
+    out=glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::ortho(l,r,b,t,n,f)* glm::mat4(1,0,0,0,0,1,0,0,-ro*(float)cos(alpha * M_PI / 180.0),-ro*(float)sin(alpha * M_PI / 180.0),1,0,0,0,0,1);
+    return out;
 }
 
