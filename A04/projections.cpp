2d5ad5bc1a83510eb9dac1da742d1bf086e24e12
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-14 12:25:51 +0100

    A04

---
+++ A04/projections.cpp
@@ -0,0 +1,27 @@
+// Create a matrix for Isometric projection with the given aspect ration a
+glm::mat4 PO1(float a) {
+	glm::mat4 out = glm::mat4(1.0);
+	return out;
+}
+
+// Create a matrix for Dimnetric projection (alpha = 45 degree)
+// with the given aspect ration a
+glm::mat4 PO2(float a) {
+	glm::mat4 out = glm::mat4(1.0);
+	return out;
+}
+
+// Create a matrix for Trimetric projection (alpha = 45 degree, beta = 60 degree)
+// with the given aspect ration a
+glm::mat4 PO3(float a) {
+	glm::mat4 out = glm::mat4(1.0);
+	return out;
+}
+
+// Create a matrix for Cabinet projection (alpha = 45)
+// with the given aspect ration a
+glm::mat4 PO4(float a) {
+	glm::mat4 out = glm::mat4(1.0);
+	return out;
+}
+
