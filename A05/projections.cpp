415c316dd33b30bd6986f2379c22f213d0b602e4
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-16 14:58:53 +0100

    A05 1-3 solved

--- A05/projections.cpp
+++ A05/projections.cpp
@@ -1,25 +1,60 @@
+//glm::mat4(1,0,0,0, 0,0,0,0, 0,0,0,0)
+//glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))
+#define _USE_MATH_DEFINES
+#include<math.h>
+//(float)tan(b * M_PI / 180.0)
+//(float)sin(b * M_PI / 180.0)
+
+float n=0.1,f=9.9;
+
+
 // Create a matrix for Perspecrtive projection with the given aspect ratio a,
 // and a FovY = 90o
+
+
 glm::mat4 PO1(float a) {
-	glm::mat4 out = glm::mat4(1.0);
+    float FovY=90;
+    float t=n*(float)tan(FovY * M_PI / 180.0);
+    float b=-1*n*(float)tan(FovY * M_PI / 180.0);
+    float l=-1*a*n*(float)tan(FovY * M_PI / 180.0);
+    float r=a*n*(float)tan(FovY * M_PI / 180.0);
+
+    glm::mat4 out1 = glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::frustum(l,r,b,t,n,f);
+    glm::mat4 out = glm::perspective(FovY,a,n,f);
+    out[1][1]*=-1;
+
 	return out;
 }
 
-// Create a matrix for Perspecrtive projection with the given aspect ratio a,
+// TODO Create a matrix for Perspecrtive projection with the given aspect ratio a,
 // and a FovY = 120o
 glm::mat4 PO2(float a) {
-	glm::mat4 out = glm::mat4(1.0);
-	return out;
+    float FovY=120;
+    float t=n*(float)tan(FovY * M_PI / 180.0);
+    float b=-1*n*(float)tan(FovY * M_PI / 180.0);
+    float l=-1*a*n*(float)tan(FovY * M_PI / 180.0);
+    float r=a*n*(float)tan(FovY * M_PI / 180.0);
+	glm::mat4 out1 = glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::frustum(l,r,b,t,n,f);
+    glm::mat4 out = glm::perspective(FovY,a,n,f);
+    out1[1][1]*=-1;
+	return out1;
 }
 
 // Create a matrix for Perspecrtive projection with the given aspect ratio a,
 // and a FovY = 30o
 glm::mat4 PO3(float a) {
-	glm::mat4 out = glm::mat4(1.0);
-	return out;
+    float FovY=30;
+    float t=n*(float)tan(FovY * M_PI / 180.0);
+    float b=-1*n*(float)tan(FovY * M_PI / 180.0);
+    float l=-a*n*(float)tan(FovY * M_PI / 180.0);
+    float r=a*n*(float)tan(FovY * M_PI / 180.0);
+    glm::mat4 out1 = glm::scale(glm::mat4(1.0),glm::vec3(1,-1,1))*glm::frustum(l,r,b,t,n,f);
+    glm::mat4 out = glm::perspective(FovY,a,n,f);
+    out[1][1]*=-1;
+	return out1;
 }
 
-// Create a matrix for Perspecrtive projection, with the given aspect ratio a.
+// TODO Create a matrix for Perspecrtive projection, with the given aspect ratio a.
 // Here the perspective should only show the left side of the view
 // with a FovY = 90o. Here r=0, and l, t and b should be computed
 // to match both the aspect ratio and the FovY
