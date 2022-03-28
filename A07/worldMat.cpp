f473fa7b218df1d25664189b72f6cf1dbe6a0a17
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-28 13:16:22 +0200

    A07 SOLVED

--- A07/worldMat.cpp
+++ A07/worldMat.cpp
@@ -12,16 +12,20 @@ float GtoR(float grades){
 // YPR.y : Pitch
 // YPR.z : Roll
 glm::mat4 MakeWorldMatrixEuler(glm::vec3 pos, glm::vec3 YPR, glm::vec3 size) {
-    glm::mat4 out = glm::translate(glm::mat4(1.0), pos) * glm::rotate(glm::mat4(1.0),
-                     GtoR(YPR.x), glm::vec3(0,1,0)) * glm::rotate(glm::mat4(1.0),
-                     GtoR(YPR.y), glm::vec3(1,0,0)) * glm::rotate(glm::mat4(1.0),
-                     GtoR(YPR.z), glm::vec3(0,0,1)) * glm::scale(glm::mat4(1.0),
-                     size);
+    glm::mat4 out = glm::translate(glm::mat4(1.0), pos) * //Translation to position
+            glm::rotate(glm::mat4(1.0), GtoR(YPR.x), glm::vec3(0,1,0)) *
+            glm::rotate(glm::mat4(1.0),GtoR(YPR.y), glm::vec3(1,0,0)) *
+            glm::rotate(glm::mat4(1.0),GtoR(YPR.z), glm::vec3(0,0,1)) * //Rotations
+            glm::scale(glm::mat4(1.0),size); //Scaling
 
     return out;
 }
 
 // Create a world matrix using position, quaternion angles, and size
+/*
+ * There are 3 ways to compute quaternions
+ * In this case we already have the rotation matrix, and we can just apply a translation + rotation + scaling
+ */
 glm::mat4 MakeWorldMatrixQuat(glm::vec3 pos, glm::quat rQ, glm::vec3 size) {
     glm::mat4 out = glm::translate(glm::mat4(1.0), pos) * glm::mat4(rQ) * glm::scale(glm::mat4(1.0),size);
 	return out;
