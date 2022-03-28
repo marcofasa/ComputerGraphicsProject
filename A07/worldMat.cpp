48a95180f8d7c7242624225dbfe639c07eca3a29
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-28 12:36:54 +0200

    A07

---
+++ A07/worldMat.cpp
@@ -0,0 +1,16 @@
+// Create a world matrix using position, Euler angles, and size
+// Euler angles are passed in YPR parameter:
+// YPR.x : Yaw
+// YPR.y : Pitch
+// YPR.z : Roll
+glm::mat4 MakeWorldMatrixEuler(glm::vec3 pos, glm::vec3 YPR, glm::vec3 size) {
+	glm::mat4 out = glm::mat4(1.0f);
+	return out;
+}
+
+// Create a world matrix using position, quaternion angles, and size
+glm::mat4 MakeWorldMatrixQuat(glm::vec3 pos, glm::quat rQ, glm::vec3 size) {
+	glm::mat4 out = glm::mat4(1.0f);
+	return out;
+}
+
