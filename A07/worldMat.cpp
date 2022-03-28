a3131a9b9276cf8b57199166544a166b10dec126
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-28 13:11:40 +0200

    A07 SOLVED

--- A07/worldMat.cpp
+++ A07/worldMat.cpp
@@ -23,7 +23,7 @@ glm::mat4 MakeWorldMatrixEuler(glm::vec3 pos, glm::vec3 YPR, glm::vec3 size) {
 
 // Create a world matrix using position, quaternion angles, and size
 glm::mat4 MakeWorldMatrixQuat(glm::vec3 pos, glm::quat rQ, glm::vec3 size) {
-	glm::mat4 out = glm::mat4(1.0f);
+    glm::mat4 out = glm::translate(glm::mat4(1.0), pos) * glm::mat4(rQ) * glm::scale(glm::mat4(1.0),size);
 	return out;
 }
 
