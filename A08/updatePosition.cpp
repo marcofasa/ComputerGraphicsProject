2d032ffb2531c5282ec6ed227acbc29575779daf
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-03-30 17:39:14 +0200

    A08 resolving

--- A08/updatePosition.cpp
+++ A08/updatePosition.cpp
@@ -11,10 +11,11 @@ namespace w
     float lastTime = 0.0f;
 }
 
+
+//Apply an Euler Matrix with only a Roll to have the correct orientation when moving
 glm::mat4 EulerRoll(glm::vec3 pos, float roll) {
     glm::mat4 out = glm::translate(glm::mat4(1.0), pos) * //Translation to position
-                    glm::rotate(glm::mat4(1.0),roll, glm::vec3(0,1,0));
-
+                    glm::rotate(glm::mat4(1.0),roll, glm::vec3(0,1,0));//Roll over
     return out;
 }
 
@@ -30,12 +31,14 @@ glm::mat4 getRobotWorldMatrix(GLFWwindow* window) {
     float deltaT = time - w::lastTime;
     w::lastTime = time;
 
+    //output
+    glm::mat4 out;
 
     //MOVEMENT <--> A west ,S south ,D right,W north
     if(glfwGetKey(window, GLFW_KEY_A)) {
         w::CamPos  -= MOVE_SPEED * glm::vec3( w::CamDir[0]) * deltaT;
-        glm::mat4 Mwvp= EulerRoll(w::CamPos,glm::radians(180.0f));
-        return Mwvp;
+        out= EulerRoll(w::CamPos,glm::radians(180.0f));
+        return out;
     }
     //No roll because by default is looking right
     if(glfwGetKey(window, GLFW_KEY_D)) {
@@ -44,23 +47,25 @@ glm::mat4 getRobotWorldMatrix(GLFWwindow* window) {
 
     if(glfwGetKey(window, GLFW_KEY_S)) {
         w:: CamPos += MOVE_SPEED * glm::vec3(w::CamDir[2]) * deltaT;
-        glm::mat4 Mwvp= EulerRoll(w::CamPos,glm::radians(-90.0f));
-        return Mwvp;
+        out= EulerRoll(w::CamPos,glm::radians(-90.0f));
+        return out;
     }
     if(glfwGetKey(window, GLFW_KEY_W)) {
         w:: CamPos -= MOVE_SPEED * glm::vec3(w::CamDir[2]) * deltaT;
-        glm::mat4 Mwvp= EulerRoll(w::CamPos,glm::radians(90.0f));
-        return Mwvp;
+        out= EulerRoll(w::CamPos,glm::radians(90.0f));
+        return out;
     }
+    //DOWN
     if(glfwGetKey(window, GLFW_KEY_F)) {
         w:: CamPos -= MOVE_SPEED * glm::vec3(w::CamDir[1]) * deltaT;
     }
+
+    //UP
     if(glfwGetKey(window, GLFW_KEY_R)) {
         w:: CamPos += MOVE_SPEED * glm::vec3(w::CamDir[1]) * deltaT;
     }
 
     //Passing to output if no Euler Roll Matrix applied
-    glm::mat4 out;
     out = glm::translate(glm::mat4(1), w::CamPos );
 
     return out;
