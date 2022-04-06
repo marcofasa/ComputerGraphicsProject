6de5de8e41018a05e3d04b8cf39930cca4435a56
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-04-06 16:32:54 +0200

    A9

--- A09/models.cpp
+++ A09/models.cpp
@@ -198,70 +198,61 @@ M3_indices.resize(sectorCount*stackCount*6);
         }
     }
 */
-int stackCount=20,sectorCount=20;
+int stackCount=12,sectorCount=12;
 
     float x, y, z, xy;                              // vertex position
     float sectorStep = 2 * M_PI / sectorCount;
     float stackStep = M_PI / stackCount;
     float sectorAngle, stackAngle;
+    int contatore=0;
     M3_vertices.resize((sectorCount*stackCount+1)*3);
-
     for(int i = 0; i <= stackCount; ++i)
     {
         stackAngle = M_PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
-        xy = radius * cos(stackAngle);             // r * cos(u)
-        z = radius * sin(stackAngle);              // r * sin(u)
+        xy = radius * cosf(stackAngle);             // r * cos(u)
+        z = radius * sinf(stackAngle);              // r * sin(u)
 
         // add (sectorCount+1) vertices per stack
         // the first and last vertices have same position and normal, but different tex coords
-        for(int j = 0; j <= sectorCount; ++j) {
+        for(int j = 0; j < sectorCount; ++j) {
             sectorAngle = j * sectorStep;           // starting from 0 to 2pi
 
             // vertex position (x, y, z)
-            x = xy * cos(sectorAngle);             // r * cos(u) * cos(v)
-            y = xy * sin(sectorAngle);             // r * cos(u) * sin(v)
+            x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
+            y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
             M3_vertices[(i*sectorCount*3)+(j*3)]=x;
             M3_vertices[(i*sectorCount*3)+(j*3)+1]=y;
             M3_vertices[(i*sectorCount*3)+(j*3)+2]=z;
-            printf("vertex %d %d \t %f %f %f \n", i ,j,x,y,z);
+            printf("vertex %d \t %f %f %f \n", contatore++,x,y,z);
         }}
 
-M3_indices.resize((sectorCount*stackCount+2)*8);
+    M3_indices.resize((sectorCount*stackCount+3)*6);
     int k1, k2;
     for(int i = 0; i < stackCount; ++i)
     {
-        k1 = (i * sectorCount) +1  ;     // beginning of current stack
-        k2 = k1 + sectorCount  ;    // beginning of next stack
+        k1 = i * (sectorCount );     // beginning of current stack
+        k2 = k1 + sectorCount;  // beginning of next stack
 
         for(int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
+            // 2 triangles per sector excluding first and last
             // k1--k1+1
             // |  / |
             // | /  |
             // k2--k2+1
-            // 2 triangles per sector excluding first and last
-            if(i != 0)
-            {
+            if (i != 0 ) {
                 M3_indices[(i*sectorCount*6)+(j*6)]=k1;
-                M3_indices[(i*sectorCount*6)+(j*6)+1]=k2+1 ;
-                M3_indices[(i*sectorCount*6)+(j*6)+2]=(k1 + 2);
+                M3_indices[(i*sectorCount*6)+(j*6)+1]=k2;
+                M3_indices[(i*sectorCount*6)+(j*6)+2]=(k1 + 1);
             }
-            else{
-                M3_indices[(j*3)]=(0);
-                M3_indices[(j*3)+1]=(k2) ;
-                M3_indices[(j*3)+2]=(k2 + 1);
-                continue;
-            }
-
-            if(i != (stackCount-1))
-            {
-                M3_indices[(i*sectorCount*6)+(j*6)+3]=(k1+1);
-                M3_indices[(i*sectorCount*6)+(j*6)+4]=(k2) ;
+            if (i != (stackCount-1)){
+                M3_indices[(i*sectorCount*6)+(j*6)+3]=(k1 + 1);
+                M3_indices[(i*sectorCount*6)+(j*6)+4]=(k2);
                 M3_indices[(i*sectorCount*6)+(j*6)+5]=(k2 + 1);
             }
 
-
-
         }}
+
+
 
 
 
