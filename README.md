d2559f07f106f0cb5a16cfaae81d52a7827e197a
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-04-06 15:16:47 +0200

    A10 solved

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ComputerGraphicsProject
 Assignments and Final project od Computer Graphics course - Polimi
+
+
+To compile  GLSL sources to the corresponding SPIR-V binaries:
+1. Copy compile.sh to the shader folder
+2. cd 'shader' folder
+3. 'chmod +x compile.sh' then './compile.sh'
---
+++ compile.sh
@@ -0,0 +1,4 @@
+/Users/fasa/VulkanSDK/1.3.204.0/macOS/bin/glslc PBRShader.vert -o PBRVert.spv
+/Users/fasa/VulkanSDK/1.3.204.0/macOS/bin/glslc PBRShader.frag -o PBRFrag.spv
+/Users/fasa/VulkanSDK/1.3.204.0/macOS/bin/glslc SkyBoxShader.vert -o SkyBoxVert.spv
+/Users/fasa/VulkanSDK/1.3.204.0/macOS/bin/glslc SkyBoxShader.frag -o SkyBoxFrag.spv
