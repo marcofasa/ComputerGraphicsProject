47b86a0ceff53c838d98d5940d4bca0eafe3adc3
Marco Fasanella <marco.fasanella@mail.polimi.it>
2022-04-06 23:04:38 +0200

    A9 solving

--- A09/models.cpp
+++ A09/models.cpp
@@ -1,8 +1,12 @@
-namespace t{
 
-}
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include <array>
+#include <unordered_map>
+#include <chrono>
 
-#define _USE_MATH_DEFINES
+
 #include<math.h>
 
 float GtoR(float grades){
@@ -13,14 +17,154 @@ float RtoG(float radians){
     return (radians * 180.0 / M_PI);
 }
 
-// this function creates the geometries to be shown, and output thems
-// in global variables M1_vertices and M1_indices to M4_vertices and M4_indices
+
+void Cylinder(float cx,float cy,float cz,int NSlices,float radius,float height,std::vector<float>& vertices,std::vector<uint32_t>& indices,bool closed, bool vertical){
+    // Vertices definitions ( SPACE NEEDED -> (NSlices+1)*6 )
+
+    if (vertical){
+    vertices.push_back (cx);
+    vertices.push_back (cy + height);
+    vertices.push_back (cz);
+    vertices.push_back (cx);
+    vertices.push_back (cy - height);
+    vertices.push_back (cz);
+
+
+    for(int i=0;i<NSlices;i++){
+
+        //Top Vertex
+        vertices.push_back (cx+radius*cos((float)(i*2.0*M_PI/NSlices)));
+        vertices.push_back (cy+height);
+        vertices.push_back (cz+radius*sin((float)(i*2.0*M_PI/NSlices)));
+
+        //Bottom Vertexes
+        vertices.push_back ( cx+radius*cos((float)(i*2.0*M_PI/NSlices)));
+        vertices.push_back ( cy-height);
+        vertices.push_back ( cz+radius*sin((float)(i*2.0*M_PI/NSlices)));
+     }
+    }
+    else {
+        vertices.push_back(cx + height);
+        vertices.push_back(cy);
+        vertices.push_back(cz);
+        vertices.push_back(cx - height);
+        vertices.push_back(cy);
+        vertices.push_back(cz);
+
+        for (int i = 0; i < NSlices; i++) {
+
+            //Top Vertex
+            vertices.push_back(cx + height);
+            vertices.push_back(cy + radius * cos((float) (i * 2.0 * M_PI / NSlices)));
+            vertices.push_back(cz + radius * sin((float) (i * 2.0 * M_PI / NSlices)));
+
+            //Bottom Vertexes
+            vertices.push_back(cx - height);
+            vertices.push_back(cy + radius * cos((float) (i * 2.0 * M_PI / NSlices)));
+            vertices.push_back(cz + radius * sin((float) (i * 2.0 * M_PI / NSlices)));
+        }
+    }
+
+// Indices definition ( SPACE NEEDED -> 12* NSlices )
+    int a=0,b=1,c=2,d=3;
+    for (int i=0;i<NSlices;i++){
+
+        //TOP
+        if(closed){
+        indices.push_back (0);
+        indices.push_back ((a% ((NSlices)*2)) +2);
+        indices.push_back ((c% ((NSlices)*2)) +2);}
+
+        //BODY
+        indices.push_back ((a% ((NSlices)*2)) +2);
+        indices.push_back ((b% ((NSlices)*2)) +2);
+        indices.push_back ((d% ((NSlices)*2)) +2);
+        indices.push_back ((a% ((NSlices)*2)) +2 );
+        indices.push_back ((c% ((NSlices)*2)) +2);
+        indices.push_back ((d% ((NSlices)*2)) +2);
+
+        //BOTTOM
+        if(closed){
+        indices.push_back (1);
+        indices.push_back ((b% ((NSlices)*2)) +2 );
+        indices.push_back ((d % ((NSlices)*2)) +2);}
+
+        a=a+2;
+        b=b+2;
+        c=c+2;
+        d=d+2;
+    }
+}
+
+void Sphere(int stackCount,int sectorCount,float radius,std::vector<float>& vertices,std::vector<uint32_t>& indices){
+    float x, y, z, xy;                              // vertex position
+    float sectorStep = 2 * M_PI / sectorCount;
+    float stackStep = M_PI / stackCount;
+    float sectorAngle, stackAngle;
+
+    //VERTEX definition (SPACE NEEDED -> ((sectorCount+1)*(stackCount+1))*3) )
+    for(int i = 0; i <= stackCount; ++i)
+    {
+        stackAngle = (M_PI / 2) - (i * stackStep);        // starting from pi/2 to -pi/2
+        xy = radius * cosf(stackAngle);             // r * cos(u)
+        z = radius * sinf(stackAngle);              // r * sin(u)
+        // printf("-------\n");
+
+        // add (sectorCount+1) vertices per stack
+        // the first and last vertices have same position and normal, but different tex coords
+        for(int j = 0; j <= sectorCount; ++j) {
+            sectorAngle = j * sectorStep;           // starting from 0 to 2pi
+
+            // vertex position (x, y, z)
+            x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
+            y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
+            vertices.push_back (x);
+            vertices.push_back (y);
+            vertices.push_back (z);
+            //printf("vertex \t %f %f %f \n", x,y,z);
+        }
+
+    }
+
+    //Indices definition ( SPACE NEEDED -> ((sectorCount*stackCount)*6) )
+    int k1, k2;
+    for(int i = 0; i < stackCount; ++i)
+    {
+        k1 = i * (sectorCount+1);     // beginning of current stack
+        k2 = (i+1)* (sectorCount+1) ;  // beginning of next stack
+        for(int j = 0; j <sectorCount; ++j, ++k1, ++k2) {
+            // 2 triangles per sector excluding first and last
+            // k1--k1+1
+            // |  / |
+            // | /  |
+            // k2--k2+1
+
+            if (i != 0 ) {
+                indices.push_back (k1+1) ;
+                indices.push_back (k2) ;
+                indices.push_back (k1);
+            }
+            else{
+                //  printf("indices  with k1=%d and k2=%d \n " , k1,k2);
+            }
+            if (i != (stackCount-1)){
+                indices.push_back (k1+1);
+                indices.push_back (k2);
+                indices.push_back (k2+1);
+            }
+            else{
+                // printf("indices with k1=%d and k2=%d \n " , k1,k2);
+            }
+
+
+
+        }}
+}
+
+
 void makeModels() {
 //// M1 : Cube
-// Replace the code below, that creates a simple square, with the one to create a cube.
 
-// Resizes the vertices array. Repalce the values with the correct number of
-// vertices components (3 * number of vertices==8)
 M1_vertices.resize(3 * 8);
 
     M1_vertices= {0,0,0,  0,0,1, // Vertex array:
@@ -46,286 +190,20 @@ M1_indices.resize(3*12);
 
 
 //// M2 : Cylinder
-// Replace the code below, that creates a simple rotated square, with the one to create a cylinder.
 
-// Resizes the vertices array. Repalce the values with the correct number of
-// vertices components (3 * number of vertices)
-/*
- * TOP Cap
- *
- *
- *nsclice+1 needed
- *
- *
- *
- *
- *
- */
-
-int NSlices=33;
-float radius = 1;
-float height =1;
-float cx=0.0,cy=0.0,cz=-3.0;
-M2_vertices.resize((NSlices+1)*6);
-
-// Vertices definitions
-M2_vertices[0] = cx;
-M2_vertices[1] = cy + height;
-M2_vertices[2] = cz;
-
-    M2_vertices[3] = cx;
-    M2_vertices[4] = cy - height;
-    M2_vertices[5] = cz;
-
-for(int i=0;i<NSlices;i++){
-    M2_vertices[(i+1)*6 +0]= cx+radius*cos((float)(i*2.0*M_PI/NSlices));
-    M2_vertices[(i+1)*6 +1]= cy+height;
-    M2_vertices[(i+1)*6 +2]= cz+radius*sin((float)(i*2.0*M_PI/NSlices));
-    //----
-    M2_vertices[(i+1)*6 +3]= cx+radius*cos((float)(i*2.0*M_PI/NSlices));
-    M2_vertices[(i+1)*6 +4]= cy-height;
-    M2_vertices[(i+1)*6 +5]= cz+radius*sin((float)(i*2.0*M_PI/NSlices));
-}
-
-// Resizes the indices array. Repalce the values with the correct number of
-// indices (3 * number of triangles)
-M2_indices.resize(12* NSlices);
-int a=0,b=1,c=2,d=3;
-    for (int i=0;i<NSlices;i++){
-        M2_indices[i*12]=0;
-        M2_indices[i*12+1]=(a% ((NSlices)*2)) +2;
-
-        //At the last iteration we must return to zero index because we are going overflow with the number of vertex
-        M2_indices[i*12+2]=(c% ((NSlices)*2)) +2;
-        //Triangolo sopra
-
-
-        M2_indices[i*12+3]=(a% ((NSlices)*2)) +2;
-        M2_indices[i*12+4]=(b% ((NSlices)*2)) +2;
-        //At the last iteration we must return to zero index because we are going overflow with the number of vertex
-        M2_indices[i*12+5]=(d% ((NSlices)*2)) +2;
-
-        M2_indices[i*12+6]=(a% ((NSlices)*2)) +2 ;
-        M2_indices[i*12+7]=(c% ((NSlices)*2)) +2;
-        //At the last iteration we must return to zero index because we are going overflow with the number of vertex
-        M2_indices[i*12+8]=(d% ((NSlices)*2)) +2 ;
-//Quadrato
-
-        M2_indices[i*12+9]=1;
-        M2_indices[i*12+10]=(b% ((NSlices)*2)) +2 ;
-        //At the last iteration we must return to zero index because we are going overflow with the number of vertex
-        M2_indices[i*12+11]=(d % ((NSlices)*2)) +2;
-
-        a=a+2;
-        b=b+2;
-        c=c+2;
-        d=d+2;
-    }
-
+    Cylinder(0.0,0.0,-3.0,33,1,1,M2_vertices,M2_indices,true,true);
 
 
 //// M3 : Sphere
-// Resizes the vertices array. Repalce the values with the correct number of
-// vertices components (3 * number of vertices)
 
-/*
-    float x, y, z;                             // vertex position
-    float stackCount=40;
-    float sectorCount=40;
-    float sectorStep = 2 * M_PI / sectorCount;
-    float stackStep = M_PI / stackCount;
-    float theta, stackAngle;
-    float r=2;
-    float phi;
-    M3_vertices.resize((sectorCount*stackCount)*3*4);
-    for(int i = 0; i <= stackCount; ++i)
-    {
-        phi = (M_PI / 2) - (stackStep * i));        // starting from pi/2 to -pi/2
-        z= r*sinf(phi);
-        // add (sectorCount+1) vertices per stack
-        // the first and last vertices have same position and normal, but different tex coords
-        for(int j = 0; j <= sectorCount; ++j) {
-            theta = j * sectorStep;           // starting from 0 to 2pi
-
-            // vertex position (x, y, z)
-            x = r * cosf(phi) * cosf(theta);             // r * cos(u) * cos(v)
-            y = r * cosf(phi) * sinf(theta);             // r * cos(u) * sin(v)
-
-
-            M3_vertices.push_back(x);
-            M3_vertices.push_back(y);
-            M3_vertices.push_back(z);
-
-
-        }}
-
-
-
-// generate CCW index list of sphere triangles
-// k1--k1+1
-// |  / |
-// | /  |
-// k2--k2+1
-
-// Resizes the indices array. Replace the values with the correct number of
-// indices (3 * number of triangles)
-M3_indices.resize(sectorCount*stackCount*6);
-
-    int k1, k2;
-    for(int i = 0; i < stackCount; ++i)
-    {
-        k1 = i * (sectorCount + 1);     // beginning of current stack
-        k2 = k1 + sectorCount + 1;      // beginning of next stack
-
-        for(int j = 0; j < sectorCount; ++j, ++k1, ++k2)
-        {
-            // 2 triangles per sector excluding first and last stacks
-            // k1 => k2 => k1+1
-            if(i != 0)
-            {
-                M3_indices.push_back(k1);
-                M3_indices.push_back(k2);
-                M3_indices.push_back(k1 + 1);
-            }
-
-            // k1+1 => k2 => k2+1
-            if(i != (stackCount-1))
-            {
-                M3_indices.push_back(k1 + 1);
-                M3_indices.push_back(k2);
-                M3_indices.push_back(k2 + 1);
-            }
-        }
-    }
-*/
-int stackCount=10,sectorCount=10;
-    int cradius=2;
-    float x, y, z, xy;                              // vertex position
-    float sectorStep = 2 * M_PI / sectorCount;
-    float stackStep = M_PI / stackCount;
-    float sectorAngle, stackAngle;
-    int contatore=0;
-    int vIndex=0;
-    M3_vertices.resize(((sectorCount+1)*(stackCount+1))*3);
-    for(int i = 0; i <= stackCount; ++i)
-    {
-        stackAngle = (M_PI / 2) - (i * stackStep);        // starting from pi/2 to -pi/2
-        xy = cradius * cosf(stackAngle);             // r * cos(u)
-        z = cradius * sinf(stackAngle);              // r * sin(u)
-        printf("-------\n");
-
-        // add (sectorCount+1) vertices per stack
-        // the first and last vertices have same position and normal, but different tex coords
-        for(int j = 0; j <= sectorCount; ++j) {
-            sectorAngle = j * sectorStep;           // starting from 0 to 2pi
-
-            // vertex position (x, y, z)
-            x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
-            y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
-            M3_vertices[vIndex]=x;
-            M3_vertices[vIndex+1]=y;
-            M3_vertices[vIndex+2]=z;
-            vIndex=vIndex+3;
-            printf("vertex %d \t %f %f %f \n", contatore++,x,y,z);
-        }
-
-    }
-
-    M3_indices.resize((sectorCount*stackCount)*6);
-    int k1, k2,index=0;
-    for(int i = 0; i < stackCount; ++i)
-    {
-        k1 = i * (sectorCount+1);     // beginning of current stack
-        k2 = (i+1)* (sectorCount+1) ;  // beginning of next stack
-        for(int j = 0; j <sectorCount; ++j, ++k1, ++k2) {
-            // 2 triangles per sector excluding first and last
-            // k1--k1+1
-            // |  / |
-            // | /  |
-            // k2--k2+1
-
-            if (i != 0 ) {
-                M3_indices[index]=(k1+1) ;
-                M3_indices[index+1]=(k2) ;
-                M3_indices[index+2]=(k1);
-                index=index+3;
-            }
-            else{
-                printf("indices %d with k1=%d and k2=%d \n " , index,k1,k2);
-            }
-            if (i != (stackCount-1)){
-                M3_indices[index]=(k1+1);
-                M3_indices[index+1]=(k2);
-                M3_indices[index+2]=(k2+1);
-                index=index+3;
-            }
-            else{
-                printf("indices %d with k1=%d and k2=%d \n " , index,k1,k2);
-            }
-
-
-
-        }
-
-    }
-
-
-
+    Sphere(10,10,2,M3_vertices,M3_indices);
 
 
 
 //// M4 : Spring
 // Replace the code below, that creates a simple octahedron, with the one to create a spring.
-M4_vertices.resize(3 * 6);
+for(int i=0;i<10;i++) {
+    Cylinder(-i, i, -i*3, 33, 1, 1, M4_vertices, M4_indices, true, false);
+}
 
-// Vertices definitions
-M4_vertices[0]  =  0.0;
-M4_vertices[1]  =  1.414;
-M4_vertices[2]  = -1.0;
-M4_vertices[3]  =  0.0;
-M4_vertices[4]  = -1.414;
-M4_vertices[5]  = -1.0;
-M4_vertices[6]  = -1.0;
-M4_vertices[7]  =  0.0;
-M4_vertices[8]  = -2.0;
-M4_vertices[9]  = -1.0;
-M4_vertices[10] =  0.0;
-M4_vertices[11] =  0.0;
-M4_vertices[12] =  1.0;
-M4_vertices[13] =  0.0;
-M4_vertices[14] =  0.0;
-M4_vertices[15] =  1.0;
-M4_vertices[16] =  0.0;
-M4_vertices[17] = -2.0;
-
-
-// Resizes the indices array. Repalce the values with the correct number of
-// indices (3 * number of triangles)
-M4_indices.resize(3 * 8);
-
-// indices definitions
-M4_indices[0]  = 0;
-M4_indices[1]  = 2;
-M4_indices[2]  = 3;
-M4_indices[3]  = 1;
-M4_indices[4]  = 3;
-M4_indices[5]  = 2;
-M4_indices[6]  = 0;
-M4_indices[7]  = 3;
-M4_indices[8]  = 4;
-M4_indices[9]  = 1;
-M4_indices[10] = 4;
-M4_indices[11] = 3;
-M4_indices[12] = 0;
-M4_indices[13] = 4;
-M4_indices[14] = 5;
-M4_indices[15] = 1;
-M4_indices[16] = 5;
-M4_indices[17] = 4;
-M4_indices[18] = 0;
-M4_indices[19] = 5;
-M4_indices[20] = 2;
-M4_indices[21] = 1;
-M4_indices[22] = 2;
-M4_indices[23] = 5;
 }
